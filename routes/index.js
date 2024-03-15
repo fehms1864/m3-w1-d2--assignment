00eff6e936f9a18ac99893fffa36660c2db3fd4d
Fehmina <fehmina.hasan@gmail.com>
2024-03-14 19:29:39 -0700

    week 2 day 3 lab: added mongo database and implement registrations

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
 const {check, validationResult} = require('express-validator');
+const { route } = require('../app');
+const Registration = mongoose.model('Registration');
+const path = require('path');
+const auth = require('http-auth');
+
+const basic = auth.basic({
+    file: path.join(__dirname, '../users.htpasswd'),
+});
 
 router.get('/', function(req, res) {
     res.render('form', {title: 'Registration form'});
@@ -18,7 +27,13 @@ router.post('/',
         // console.log(req.body);
         const errors = validationResult(req);
         if(errors.isEmpty()){
-            res.send('Thank you for your registration!');
+            const registration = new Registration(req.body);
+            registration.save()
+            .then(() => {res.send('Thank you for your registration!'); })
+            .catch((err) => {
+                console.log(err);
+                res.send('Sorry! Something went wrong.');
+            });
         } else {
             res.render('form', {
                 title: 'Registration form',
@@ -29,5 +44,13 @@ router.post('/',
         // res.render('form', {title: 'Registration form'});
     }
 );
+
+router.get('/registrations', basic.check((req, res) => {
+    Registration.find()
+    .then((registrations) => {
+        res.render('index', {title: 'Listing registrations', registrations });
+    })
+    .catch(() => { res.send('Sorry! Something went wrong.'); });
+}));
 
 module.exports = router;
